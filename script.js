2ca5dad5644a84e650000917cd307bb60df9ac97
axe_mello <akselcruses@gmail.com>
2024-03-28 18:00:02 +0530

    Begining

---
+++ script.js
@@ -0,0 +1,15 @@
+// browser
+
+console.log(document.getElementById("head"));
+
+
+document.addEventListener("DOMContentLoaded",
+    function(event) {
+        console.log(event);
+
+
+    function testing (event) {
+    };
+
+
+});
